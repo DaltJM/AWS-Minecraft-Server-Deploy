401a2a0c74179fd45320c74c60773a5bb7b37a21
Dalton Mein <ic3yt0uch@gmail.com>
2019-10-22 13:03:02 +1100

    New pages added
    Javascript added to own file, JS removed from HTML. Link to AWS added. Text formatting added to text on each page.

---
+++ MyScript.js
@@ -0,0 +1,52 @@
+
+
+window.onload = function () {
+
+    /* When the user clicks on the button,
+    toggle between hiding and showing the dropdown content */
+    function myFunction() {
+        document.getElementById("myDropdown").classList.toggle("show");
+    }
+
+    // Close the dropdown menu if the user clicks outside of it
+    window.onclick = function (event) {
+        if (!event.target.matches('.dropbtn')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
+    var slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {
+            slideIndex = 1
+        }
+        slides[slideIndex - 1].style.display = "block";
+        setTimeout(showSlides, 5000); // Change image every 2 seconds
+    }
+
+
+}
+// tell the embed parent frame the height of the content
+if (window.parent && window.parent.parent) {
+    window.parent.parent.postMessage(["resultsFrame", {
+        height: document.body.getBoundingClientRect().height,
+        slug: "0kx925qs"
+    }], "*")
+}
+
+// always overwrite window.name, in case users try to set it manually
+window.name = "result"
---
+++ MyScripts.js
@@ -0,0 +1,44 @@
+<script type="text/javascript">//<![CDATA[
+
+    window.onload = function () {
+
+        /* When the user clicks on the button,
+        toggle between hiding and showing the dropdown content */
+        function myFunction() {
+            document.getElementById("myDropdown").classList.toggle("show");
+        }
+
+      // Close the dropdown menu if the user clicks outside of it
+      window.onclick = function (event) {
+        if (!event.target.matches('.dropbtn')) {
+          var dropdowns = document.getElementsByClassName("dropdown-content");
+var i;
+          for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+}
+}
+}
+}
+var slideIndex = 0;
+showSlides();
+
+      function showSlides() {
+        var i;
+var slides = document.getElementsByClassName("mySlides");
+        for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+}
+slideIndex++;
+        if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 5000); // Change image every 2 seconds
+  }
+
+
+}
+
+  //]]></script>
